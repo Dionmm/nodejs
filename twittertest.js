a1e8bfa4b76633fbb5618f4f27e369c8b4a66e0d
dion <dion@macintyres.me.uk>
2015-12-05 17:23:42 +0000

    creating twitter feed

---
+++ twittertest.js
@@ -0,0 +1,20 @@
+/**
+ * Created by dion on 05/12/2015.
+ */
+var Twitter = require('twitter');
+var http = require('http');
+var port = process.env.PORT || 1337
+
+var client = new Twitter({
+    consumer_key: 'AfbvSlccLW6UfML1TEmNf604J',
+    consumer_secret: 'h3HJ6tpITLKoCE3QVul7v8sPRowZPZwu1HEH77hBwxCXT70SgA',
+    access_token_key: '25139231-T4ToRmBomtuIt9VN5ImjFHX3OW6kY4wRtzmfT1gW4',
+    access_token_secret: 'cvwZzssbqMBCHHCJSWybxIEq4fCj0s4sWLaslUavNz7oP'
+});
+http.createServer(function(request, response){
+    response.wreiteHead(200, {'Content-Type': 'text/plain'});
+
+    client.get('search/tweets',{q: 'lolcat', count:'2'}, function(error, tweets){
+        console.log(tweets);
+    });
+}).listen(port);
