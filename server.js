f22ab3b4fa8507489df3d55570d9bd7ab5ffe227
dion <dion@macintyres.me.uk>
2015-12-20 20:03:16 +0000

    moving stuff to server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-require('./twittertest');
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: 'AfbvSlccLW6UfML1TEmNf604J',
+    consumer_secret: 'h3HJ6tpITLKoCE3QVul7v8sPRowZPZwu1HEH77hBwxCXT70SgA',
+    access_token_key: '25139231-T4ToRmBomtuIt9VN5ImjFHX3OW6kY4wRtzmfT1gW4',
+    access_token_secret: 'cvwZzssbqMBCHHCJSWybxIEq4fCj0s4sWLaslUavNz7oP'
+});
+
+client.get('search/tweets',{q: 'lolcat'}, function(error, tweets){
+    console.log(tweets);
+    console.log(error);
+});
