e70e3adc238357fca56a9288e34923e73c871526
dion <dion@macintyres.me.uk>
2015-12-20 21:44:35 +0000

    attempt three to search based on a username

--- twittertest.js
+++ twittertest.js
@@ -5,7 +5,7 @@ var Twitter = require('twitter');
 var http = require('http');
 var port = process.env.PORT || 1337;
 var url = require('url');
-var queryData = url.parse(request.url, true);
+var queryData = url.parse(request.url, true).query;
 var search = queryData.q;
 
 var client = new Twitter({
@@ -14,19 +14,20 @@ var client = new Twitter({
     access_token_key: '25139231-T4ToRmBomtuIt9VN5ImjFHX3OW6kY4wRtzmfT1gW4',
     access_token_secret: 'cvwZzssbqMBCHHCJSWybxIEq4fCj0s4sWLaslUavNz7oP'
 });
+if(search){
+    http.createServer(function(request, response) {
+        response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*' });
 
-http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*' });
+        client.get('statuses/user_timeline', {screen_name: search, count:'10'}, function(error, tweets){
 
-    client.get('statuses/user_timeline', {screen_name: search, count:'10'}, function(error, tweets){
+            var json = [];
+            for (var i =0; i< tweets.length ; i++)
+            {
+                json.push({username: tweets[i].user.screen_name, image: tweets[i].user.profile_image_url, text: tweets[i].text});
+            }
 
-        var json = [];
-        for (var i =0; i< tweets.length ; i++)
-        {
-            json.push({username: tweets[i].user.screen_name, image: tweets[i].user.profile_image_url, text: tweets[i].text});
-        }
+            response.end(JSON.stringify(json));
+        });
 
-        response.end(JSON.stringify(json));
-    });
-
-}).listen(port);
+    }).listen(port);
+}
