869d28fd7ac93e566a5ad38e3b9e94894d27826b
dion <dion@macintyres.me.uk>
2015-12-20 20:30:57 +0000

    filtering the data i need

--- twittertest.js
+++ twittertest.js
@@ -16,6 +16,13 @@ http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*' });
 
     client.get('search/tweets', {q: 'lolcats', count:'2'}, function(error, tweets){
+
+        var json = [];
+        for (var i =0; i< tweets.statuses.length ; i++)
+        {
+            json.push({name: tweets.statuses[i].user.name, text: tweets.statuses[i].text});
+        }
+
         response.end(JSON.stringify(tweets));
     });
 
