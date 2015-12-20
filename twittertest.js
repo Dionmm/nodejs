4a125de8075f2c8cfad9f60c427171347b6952ad
dion <dion@macintyres.me.uk>
2015-12-20 20:00:06 +0000

    removing stuff

--- twittertest.js
+++ twittertest.js
@@ -11,10 +11,8 @@ var client = new Twitter({
     access_token_key: '25139231-T4ToRmBomtuIt9VN5ImjFHX3OW6kY4wRtzmfT1gW4',
     access_token_secret: 'cvwZzssbqMBCHHCJSWybxIEq4fCj0s4sWLaslUavNz7oP'
 });
-http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    client.get('search/tweets',{q: 'lolcat'}, function(error, tweets){
-        console.log(tweets);
-    });
-}).listen(port);
+client.get('search/tweets',{q: 'lolcat'}, function(error, tweets){
+    console.log(tweets);
+    console.log(error);
+});
