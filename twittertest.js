985cabd73e022a876de74e08c3bc20fa434be820
dion <dion@macintyres.me.uk>
2015-12-20 20:47:49 +0000

    changing query

--- twittertest.js
+++ twittertest.js
@@ -15,7 +15,7 @@ var client = new Twitter({
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*' });
 
-    client.get('search/tweets', {q: 'politics', count:'5'}, function(error, tweets){
+    client.get('search/tweets', {q: 'dionmm', count:'5'}, function(error, tweets){
 
         var json = [];
         for (var i =0; i< tweets.statuses.length ; i++)
